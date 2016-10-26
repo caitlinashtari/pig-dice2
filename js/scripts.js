90fe5a5ad9c8b1dee4185387e14d954f00c37813
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-10-26 14:27:24 -0700

    attempt to solve the problem of adding current score to previous score. Unsuccessful.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 /* backend ------------*/
 var roll1;
 var roll2;
-var roll3;
-var roll4;
 var player1;
 var player2;
+var score;
 
 //constructor function for player
 function Player () {
@@ -29,47 +28,48 @@ var roll = function (){
 
 //player prototype to be called on player to change status; need to incorporate conditional statement from frontend here
 Player.prototype.activeStatus = function() {
-  if((roll1) != 1 || (roll2 != 1)){
+  if((roll1 != 1) || (roll2 != 1)){
     this.active = true;
+    score = player1.score + roll1 + roll2;
+    this.score = score;
+  } else if((roll1 === 1) && (roll2 != 1)){
+    this.active = false;
+    return this.score;
+  } else if((roll1 === 1) && (roll2 === 1)){
+    this.active = false;
+    return this.score = 0;
   }
 }
 
+var player = function(){
+  if("player1.active === true"){
+    currentPlayer = player1;
+    return player1.activeStatus();
+  } else {
+    currentPlayer = player2;
+    return player2.activeStatus();
+  }
+}
 
 /* frontend ------------*/
 $(document).ready(function(){
 //used .one() method so that on first click, new Player is created, but only on the first click; need to change screen so that player will be prompted to click again.
-  var clicks = 0;
-  $("button.btn").one("click", function(event){
+  $("button#clickOne").click(function(event){
     event.preventDefault();
     player1 = new Player();
     player2 = new Player();
     player1.activeStatus();
+    $("form#page1").hide();
+    $("div.row").show();
   });
 
 //need to change screen so that player is prompted to click again; this includes prototype that needs to be moved to backend
-  $("button.btn").click(function(){
-    if((clicks === 0) && (this.active === true)){
-
+  $("button#play").click(function(event){
+    event.preventDefault();
     roll1 = roll();
     roll2 = roll();
-      if((roll1 != 1) && (roll2 != 1)){
-      this.score += roll1 + roll2;
-    } else {
-      this.score = this.score;
-      this.active = false;
-    }
+    player();
 
-  } else if((clicks >= 1) && (this.active === true)){
-    roll3 = roll();
-    roll4 = roll();
-      if((roll1 != 1) && (roll2 != 1)){
-        thid.score += this.score + roll3 + roll4;
-      } else {
-        this.score = this.score;
-        this.active = false;
-      }
-  }
-  ++clicks;
 
 });
 
