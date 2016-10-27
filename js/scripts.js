83dd2bb5bc6bc7ce3bea61ef69d84a87a65249fb
caitlin ashtari and haley swain <haley@>
2016-10-26 20:25:35 -0700

    create functionality for the game

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ var roll1;
 var roll2;
 var player1;
 var player2;
-var score;
+var currentPlayer;
 
 //constructor function for player
-function Player () {
+function Player() {
   this.score = 0;
   this.active = false;
 }
 //this is just so i can visualize the player objects that are created
 // player1 = {
 //   this.score = 0;
-//   this.active = true;
+//   this.active = false;
 // }
 //
 // player2 = {
@@ -26,28 +26,30 @@ var roll = function (){
   return Math.floor(Math.random() * 6) + 1;
 }
 
+
 //player prototype to be called on player to change status; need to incorporate conditional statement from frontend here
 Player.prototype.activeStatus = function() {
-  if((roll1 != 1) || (roll2 != 1)){
-    this.active = true;
-    score = player1.score + roll1 + roll2;
-    this.score = score;
-  } else if((roll1 === 1) && (roll2 != 1)){
-    this.active = false;
-    return this.score;
-  } else if((roll1 === 1) && (roll2 === 1)){
-    this.active = false;
-    return this.score = 0;
+  if((roll1 === 1) && (roll2 === 1)){
+    changeStatus();
+    this.score = 0;
+
+  } else if(((roll1 === 1) && (roll2 !== 1))|| ((roll1 !== 1) && (roll2 === 1))){
+    changeStatus();
+
+  } else if((roll1 !== 1) || (roll2 !== 1)){
+    this.score += (roll1 + roll2);
   }
 }
 
-var player = function(){
-  if("player1.active === true"){
-    currentPlayer = player1;
-    return player1.activeStatus();
-  } else {
+var changeStatus = function(){
+  if(player1.active === true){
+    player1.active = false;
+    player2.active = true;
     currentPlayer = player2;
-    return player2.activeStatus();
+  } else {
+    player2.active = false;
+    player1.active = true;
+    currentPlayer = player1;
   }
 }
 
@@ -58,7 +60,8 @@ $(document).ready(function(){
     event.preventDefault();
     player1 = new Player();
     player2 = new Player();
-    player1.activeStatus();
+    player1.active = true;
+    currentPlayer = player1;
     $("form#page1").hide();
     $("div.row").show();
   });
@@ -68,7 +71,7 @@ $(document).ready(function(){
     event.preventDefault();
     roll1 = roll();
     roll2 = roll();
-    player();
+    currentPlayer.activeStatus();
 
 
 });
